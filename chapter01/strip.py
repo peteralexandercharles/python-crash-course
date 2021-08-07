e260b83ff2a925ca0de226578abb8a60f1a9971a
Peter Alexander Charles Oliver <peteralexandercharles@gmail.com>
2021-08-07 20:43:04 -0300

    Using strip to remove whitespace

---
+++ chapter01/strip.py
@@ -0,0 +1,13 @@
+favorite_language = "python "
+print(favorite_language)
+print(favorite_language.rstrip()) # remove the whitespace from right side
+message = favorite_language.rstrip()
+print(favorite_language)
+
+java = " Java"
+print(java)
+print(java.lstrip()) # remove the whitespace from left side
+
+django = " django "
+print(django)
+print(django.strip()) # remove the whitespace from both sides
