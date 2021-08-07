6b85150443d5fa8e8358a87a114a3a1aba86a44f
Peter Alexander Charles Oliver <peteralexandercharles@gmail.com>
2021-08-07 20:32:19 -0300

    whitespage

---
+++ chapter01/whitespage.py
@@ -0,0 +1,11 @@
+print("Python")
+
+# Adding Whitespace
+print("\tPython")
+
+# Adding a newline in a string
+print("Languages:\nPython\nC\nJavaScript")
+
+# Adding a newline in a string and tabs
+print("Languages:\n\tPython\n\tC\n\tJavaScript") 
+
