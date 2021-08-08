5d4e455034c93eb93f7bc6143029354046eee4dd
Peter Alexander Charles Oliver <peteralexandercharles@gmail.com>
2021-08-08 09:27:12 -0300

    Using Individual Values from a List

---
+++ chapter03/bicycles.py
@@ -0,0 +1,6 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
+print(bicycles[3])
+print(bicycles[-1])
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
